013fe6045a955b0fb28cea9ea95cdd3ad537282e
Magiczny Czarodziej <nexit07@gmail.com>
2019-07-25 20:05:46 +0200

    Add first section of portfolio
    Created "about" section, added feather icons sprite and font weight
    variant (100)

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,12 +1,22 @@
-import React, { Component } from "react"
+import React, { Component } from 'react';
+import styled from 'styled-components';
 import GlobalStyle from '../components/GlobalStyle';
+import AboutSection from '../components/AboutSection';
+
+const Wrapper = styled.div`
+  height: 100vh;
+  scroll-behavior: smooth;
+  overflow-y: scroll;
+  scroll-snap-type: y proximity;
+`;
 
 export default class Portfolio extends Component {
   render() {
     return (
-      <>
+      <Wrapper>
         <GlobalStyle/>
-      </>
+        <AboutSection/>
+      </Wrapper>
     );
   }
 }
